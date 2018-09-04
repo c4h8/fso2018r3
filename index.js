c1f9c6ad633fdc5e28980d610b4e6e51c3c35e7d
c4h8 <panu.liesio@gmail.com>
2018-09-04 22:45:14 +0300

    complete assignments 3.17 and 3.18

--- index.js
+++ index.js
@@ -14,41 +14,6 @@ app.use(bodyParser.json());
 morgan.token('payload', req => JSON.stringify(req.body));
 app.use(morgan(':method :url :payload :status :res[content-length] - :response-time ms'));
 
-let data = {
-  "persons": [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Martti Tienari",
-      "number": "040-123456",
-      "id": 2
-    },
-    {
-      "name": "Arto Järvinen",
-      "number": "040-123456",
-      "id": 3
-    },
-    {
-      "name": "Lea Kutvonen",
-      "number": "040-123456",
-      "id": 4
-    }
-  ]
-};
-
-const generateId = () => Math.floor(Math.random() * 1000000000);
-const getIds = () => data.persons.map(p => p.id);
-const getNames = () => data.persons.map(p => p.name);
-const nameIsNotUnique = name => getNames().includes(name);
-const addPerson = payload => {
-  const newPerson = Object.assign({}, payload, {id: generateId()})
-  data.persons.push(newPerson);
-  return (newPerson);
-};
-
 app.get('/api/persons', (req, res) => {
   Person
     .find({})
@@ -92,34 +57,38 @@ app.put('/api/persons/:index', (req, res) => {
   if(!payload.name || payload.name.trim() === '') return res.status(400).json({error: 'name required'});
   if(!payload.number || payload.number.trim() === '') return res.status(400).json({error: 'number required'});
 
-  const existingPerson = Person.findOne({ 'name': payload.name })
+  Person.findOneAndUpdate({ 'name': payload.name }, {number: payload.number}, { new: true })
+    .then(p => res.json(Person.format(p)))
+    .catch(e => {
+      console.log(e.message);
+      res.status(400).json({error: 'something went wrong'});
+    });
+});
 
-  if (existingPerson) {
-    existingPerson.number = payload.number;
-    existingPerson.save()
-      .then(p => res.json(Person.format(p)))
-      .catch(e => {
-        console.log(e.message);
-        res.status(400).json({error: 'something went wrong'});
-      });
-  } else {
-    res.status(404).json({error: 'person not found'});
-  }
+app.delete('/api/persons/:index', (req, res) => {
+  const id = req.params.index;
+
+  Person
+    .deleteOne({ '_id': id})
+    .then(p => res.json(Person.format(p)))
+    .catch(e => {
+      console.log(e.message)
+      res.status(404).json({error: 'person not found'});
+    });
 });
 
 app.get('/info', (req, res) => {
-  let userCount = undefined;
   const date = new Date().toString();
 
   Person
     .find({})
-    .then(persons => userCount = persons.length)
+    .then(persons =>
+      res.send(`
+        <p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+        <p>${date}<p>
+      `)
+    )
     .catch(e => res.status(500).json({error: 'something went wrong'}))
-
-  res.send(`
-    <p>puhelinluettelossa on ${data.persons.length} henkilön tiedot</p>
-    <p>${date}<p>
-  `);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
